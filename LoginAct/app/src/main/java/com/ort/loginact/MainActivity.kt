4862db15e3c0c53cbd5583ccc56b33da2d1cf977
facu3064 <santiago.marcelo.morales@gmail.com>
2023-03-23 14:18:56 -0300

    Try Login with list

--- LoginAct/app/src/main/java/com/ort/loginact/MainActivity.kt
+++ LoginAct/app/src/main/java/com/ort/loginact/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ort.loginact
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -11,24 +12,34 @@ class MainActivity : AppCompatActivity() {
     private lateinit var pass: EditText
     private lateinit var btnLogIn: Button
     private lateinit var txtView: TextView
-    private var checkMail: String = "hola"
-    private var checkPass: String = "hola"
-    private var inputTextMail: String = ""
-    private var inputTextPass: String = ""
 
+        private var emailList : MutableList<String> = mutableListOf()
+    private var passList : MutableList<String> = mutableListOf()
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnLogIn.setOnClickListener() {
-            inputTextMail = email.text.toString()
-            inputTextPass = pass.text.toString()
-            if (inputTextMail == checkMail && inputTextPass == checkPass) {
-                txtView.text = "Ingresado Correctamente"
-            }
-            else {
-                txtView.text = "E-mail o Contraseña Incorrecta"
+        email = findViewById(R.id.EmailAdress)
+        pass = findViewById(R.id.TextPassword)
+        btnLogIn = findViewById(R.id.buttonLogIn)
+        txtView = findViewById(R.id.textView)
+
+        emailList.add("numero1")
+        emailList.add("numero2")
+        emailList.add("numero3")
+
+        passList.add("pass1")
+        passList.add("pass2")
+        passList.add("pass2")
+
+        btnLogIn.setOnClickListener {
+            val checkEmail : String = email.toString()
+            val emailExists = emailList.contains(checkEmail)
+            if (emailExists){
+                txtView.text = "Hola"
             }
         }
     }
